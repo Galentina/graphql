f0989ca96ea1efab49e044eccb64d849da58e50b
Galina Malareva <gala.malareva@gmail.com>
2021-12-16 21:46:23 +0100

    app added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import {useMutation, useQuery} from '@apollo/client';
+import { GET_ALL_USERS, GET_ONE_USER } from './query/user';
+import './App.css'
+import { useForm } from 'react-hook-form';
+import { CREATE_USER } from './mutation/user';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { data, loading, error, refetch } = useQuery(GET_ALL_USERS)
+    const { data: oneUser, loading: loadingOneUser } = useQuery(GET_ONE_USER, {
+        variables: {
+            id: 1
+        }
+    })
+
+    console.log('1', oneUser);
+
+    const [newUser] = useMutation(CREATE_USER)
+    const [users, setUsers] = useState([])
+    const { handleSubmit, form, register, reset } = useForm({
+        defaultValues: {
+            username: '',
+            age: 0
+        }
+    });
+    console.log(data);
+
+
+    useEffect(() => {
+        if (!loading) {
+            setUsers(data.getAllUsers)
+        }
+    }, [data]);
+
+    if (loading) {
+        return <h1>Loading</h1>
+    }
+
+    const addUser = (username, age) => {
+        console.log(username, age)
+        newUser({
+            variables: {
+                input: {
+                    username, age
+                }
+            }
+        }).then(({data}) => {
+            console.log(data)
+        })
+    }
+
+    const getAll = (e) => {
+        e.preventDefault()
+        refetch()
+    }
+
+    const onSubmit = (data) => {addUser(String(data.username), Number(data.age))
+        console.log(data)
+
+        reset();
+    }
+
+    return (
+        <div >
+            <form onSubmit={ handleSubmit(onSubmit) }>
+                <input type='text' name='username' autoComplete='off' {...register('username')}/>
+                <input type='number' name='age' autoComplete='off' {...register('age')}/>
+                <div className='btns'>
+                    <button type='submit'>Create</button>
+                    <button onClick={(e) => getAll(e)}>Get</button>
+                </div>
+            </form>
+            <div>
+                {users.map((user, i) =>
+                    <div className='user' key={i}>{i+1} {user.username} {user.age}</div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000/graphql",
+    cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
